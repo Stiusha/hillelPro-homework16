76f9f5e953fbda205815b6fe7a969fc9cd8f53d2
stiusha <stiusha0110@gmail.com>
2026-06-01 00:53:28 +0300

    HM16 functional interfaces

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class Main {
+    public static void main(String[] args) {
+        int operate = new MathOperation() {
+            @Override
+            public int operate(int a, int b) {
+                return a + b;
+            }
+        }.operate(9, 6);
+        System.out.println(operate);
+
+        StringManipulator manipulator = (s) -> s.toUpperCase();
+        String upperedCase = manipulator.manipulate("чепуха");
+        System.out.println(upperedCase);
+
+        Function<String, Integer> function = StringListProcessor::countUppercase;
+        Integer countUpperCase = function.apply("ЧеПуХа");
+        System.out.println(countUpperCase);
+
+        Integer randomNumber = new Supplier<Integer>() {
+            @Override
+            public Integer get() {
+                return RandomNumberGenerator.generateRandomNumber(1, 100);
+            }
+        }.get();
+        System.out.println(randomNumber);
+
+
+    }
+}
---
+++ src/StringManipulator.java
@@ -0,0 +1,3 @@
+public interface StringManipulator {
+    String manipulate(String s);
+}
